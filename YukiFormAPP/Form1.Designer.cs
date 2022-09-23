a5699ffa5faa5b683a828613a03332a12618ef16
小池陽祥 <hisaki0714@outlook.jp>
2022-09-23 18:54:21 +0800

    Updated.

--- YukiFormAPP/Form1.Designer.cs
+++ YukiFormAPP/Form1.Designer.cs
@@ -31,6 +31,18 @@ namespace YukiFormAPP
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title6 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Clock.exe");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Desktop", new System.Windows.Forms.TreeNode[] {
+            treeNode21});
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Hisaki", new System.Windows.Forms.TreeNode[] {
+            treeNode22});
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("$HOME", new System.Windows.Forms.TreeNode[] {
+            treeNode23});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
@@ -42,6 +54,32 @@ namespace YukiFormAPP
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
             this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Memo = new System.Windows.Forms.RichTextBox();
+            this.Box = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.yuki = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -122,6 +160,17 @@ namespace YukiFormAPP
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
             this.comboBox1.Location = new System.Drawing.Point(212, 31);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 20);
@@ -137,19 +186,240 @@ namespace YukiFormAPP
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(3, 728);
+            this.button3.Location = new System.Drawing.Point(539, 33);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(250, 23);
+            this.button3.Size = new System.Drawing.Size(732, 23);
             this.button3.TabIndex = 14;
-            this.button3.Text = "サーバーに負担をかけたい人はこのボタンを連打";
+            this.button3.Text = "サーバーに負担をかけたい人はこのボタンをクリック(マジでサーバーに負担がかかり、このアプリがフリーズします。フリーズした場合、PCを再起動してください。)";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(3, 243);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "chart1";
+            title6.Name = "売上";
+            this.chart1.Titles.Add(title6);
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Memo
+            // 
+            this.Memo.Location = new System.Drawing.Point(5, 550);
+            this.Memo.Name = "Memo";
+            this.Memo.Size = new System.Drawing.Size(248, 160);
+            this.Memo.TabIndex = 16;
+            this.Memo.Text = "";
+            // 
+            // Box
+            // 
+            this.Box.Location = new System.Drawing.Point(1204, 699);
+            this.Box.Multiline = true;
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(100, 19);
+            this.Box.TabIndex = 17;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(212, 68);
+            this.treeView1.Name = "treeView1";
+            treeNode21.Name = "ノード11";
+            treeNode21.Text = "Clock.exe";
+            treeNode22.Name = "ノード8";
+            treeNode22.Text = "Desktop";
+            treeNode23.Name = "ノード7";
+            treeNode23.Text = "Hisaki";
+            treeNode24.Name = "ノード0";
+            treeNode24.Text = "$HOME";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode24});
+            this.treeView1.Size = new System.Drawing.Size(121, 97);
+            this.treeView1.TabIndex = 18;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(890, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 102);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(82, 30);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 19);
+            this.numericUpDown1.TabIndex = 20;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.yuki);
+            this.tabControl1.Location = new System.Drawing.Point(1092, 68);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(200, 100);
+            this.tabControl1.TabIndex = 21;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // yuki
+            // 
+            this.yuki.Location = new System.Drawing.Point(4, 22);
+            this.yuki.Name = "yuki";
+            this.yuki.Padding = new System.Windows.Forms.Padding(3);
+            this.yuki.Size = new System.Drawing.Size(192, 74);
+            this.yuki.TabIndex = 2;
+            this.yuki.Text = "tabPage3";
+            this.yuki.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.splitContainer1);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(890, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RAID";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(68, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(128, 100);
+            this.splitContainer1.SplitterDistance = 42;
+            this.splitContainer1.TabIndex = 24;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 81);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(56, 16);
+            this.radioButton4.TabIndex = 26;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "RAID5";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 63);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 16);
+            this.radioButton3.TabIndex = 25;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "RAID3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 40);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 16);
+            this.radioButton2.TabIndex = 24;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "RAID1\r\n";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 16);
+            this.radioButton1.TabIndex = 23;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "RAID0\r\n";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(539, 55);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 23);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "PCのプロセス確認";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(460, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 12);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Don\'t push.→";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 742);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 12);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "©By Hisaki Koike.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1301, 766);
+            this.ClientSize = new System.Drawing.Size(1309, 772);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.Box);
+            this.Controls.Add(this.Memo);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.hScrollBar1);
             this.Controls.Add(this.comboBox1);
@@ -163,6 +433,14 @@ namespace YukiFormAPP
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,6 +459,25 @@ namespace YukiFormAPP
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.HScrollBar hScrollBar1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RichTextBox Memo;
+        private System.Windows.Forms.TextBox Box;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage yuki;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- YukiFormAPP/Form1.cs
+++ YukiFormAPP/Form1.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 using System;
 using System.Threading;
 using System.Windows.Forms;
+using System.Diagnostics;
+
 
 namespace YukiFormAPP
 {
+
     public partial class Form1 : Form
     {
+        private int count;
+        private object proc;
+
         public Form1()
         {
             InitializeComponent();
@@ -27,7 +33,7 @@ namespace YukiFormAPP
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("This Make with Microsoft Vsual Studio Code 2019 C# Lang");
+            MessageBox.Show("This Make with Microsoft Vsual Studio Code 2019 C# Lang.");
         }
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -44,8 +50,8 @@ namespace YukiFormAPP
 
         private void label1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("YukiAPP Test Ver 0.3." +
-                "©By Hisaki Koike");
+            MessageBox.Show("YukiAPP Test Ver 0.4." +
+                "");
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -97,9 +103,66 @@ namespace YukiFormAPP
 
         private void button3_Click_1(object sender, EventArgs e)
         {
+            // whileで無限ループする
+            while (true)
+            {
+                //MessageBox.Show("負担中...");
+                if (count == 8)
+                {
+                    // ループを抜ける
+                    break;
+                }
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Don't push!!");
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
 
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello.");
+        }
 
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a RAID of HDD,SSD");
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+                    }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //object p = proc.Kill();
+            // 実行中のすべてのプロセスを取得する
+            System.Diagnostics.Process[] hProcesses = System.Diagnostics.Process.GetProcesses();
+
+            // コンピュータ名を指定すると、別のコンピュータのプロセスが取得可能です
+            //hProcesses = System.Diagnostics.Process.GetProcesses("MachineName");
+
+            string stPrompt = string.Empty;
+
+            // 取得できたプロセスからプロセス名を取得する
+            foreach (System.Diagnostics.Process hProcess in hProcesses)
+            {
+                stPrompt += hProcess.ProcessName + System.Environment.NewLine;
+            }
+
+            // 実行中のすべてのプロセス名を表示する
+            MessageBox.Show(stPrompt);
+        }
     }
 }
